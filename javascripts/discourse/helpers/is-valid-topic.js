0a56fa6c11c9227a629f8879418efb86671ce98f
marc <marc.garcia@marfeel.com>
2024-05-07 16:47:12 +0200

    private messages support

--- javascripts/discourse/helpers/is-valid-topic.js
+++ javascripts/discourse/helpers/is-valid-topic.js
@@ -1,10 +1,14 @@
 import { registerRawHelper } from 'discourse-common/lib/helpers';
 
-registerRawHelper('isValidCategory', function(value) {
+registerRawHelper('isValidTopic', function(category, isPrivateMessage) {
+	if (isPrivateMessage) {
+		return settings.private_messages;
+	}
+
 	const array = settings.categories.split('|')
 		.filter((id) => id !== '')
 		.map((id) => parseInt(id, 10))
 		.filter((id) => id);
 
-	return array.length === 0 || array.includes(value);
+	return array.length === 0 || array.includes(category);
 });
